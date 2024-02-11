a7777154269fd1da769854ae008d16eeb1159b04
gennady <gennady@sosiv.io>
2024-02-12 00:23:53 +0200

    rmv

--- server/numbers.go
+++ server/numbers.go
@@ -41,35 +41,3 @@ func Numbers(ctrl NumbersFetcher) http.HandlerFunc {
 		}
 	}
 }
-
-//func (srv *Server) HandlerValues() http.HandlerFunc {
-//	return func(w http.ResponseWriter, r *http.Request) {
-//		ctx, cancel := context.WithTimeout(r.Context(), config.DefaultTimeout)
-//		defer cancel()
-//
-//		vals, ok := r.URL.Query()["vals"]
-//		if !ok || len(urls[0]) < 1 {
-//			utils.RespondJSON(w, &ValueResponse{Numbers: []int{}})
-//		}
-//
-//		srv.ctrl.ProcessValues(vals)
-//		ready := len(urls)
-//		var old, new []int
-//		for {
-//			select {
-//			case <-ctx.Done():
-//				log.Println("Timeout")
-//				utils.RespondJSON(w, &ValueResponse{Numbers: old})
-//				return
-//			case received = <-srv.ctrl.ReadValuesExternal():
-//				old = utils.ProcessNumbers(old, new)
-//				ready--
-//				if ready == 0 {
-//					log.Println("All jobs finished")
-//					utils.RespondJSON(w, &ValueResponse{Numbers: old})
-//					return
-//				}
-//			}
-//		}
-//	}
-//}
